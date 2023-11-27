15dfa4ecc3a975faa7a4a69be40ca3bbc8dabc37
Anthony Mamode <anthony.mamode@witick.io>
2023-11-27 18:00:09 +0100

    Add tooltip on browse's operation list hovered (only if item is overflowing)

--- src/app/components/operation/OperationListItem.tsx
+++ src/app/components/operation/OperationListItem.tsx
@@ -1,9 +1,9 @@
 import { OperationLabel } from "@/app/components/typography/OperationLabel";
 import { StandaloneOperation } from "@/app/lib/model/StandaloneOperation";
 import { PushPin } from "@mui/icons-material";
-import { Box, ListItem, ListItemButton, ListItemContent, Typography } from "@mui/joy";
+import { Box, ListItem, ListItemButton, ListItemContent, Tooltip, Typography } from "@mui/joy";
 import IconButton from "@mui/joy/IconButton";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export interface OperationListItemProps {
     operation: StandaloneOperation
@@ -25,10 +25,44 @@ export const OperationListItem = ({
     onPin
 }: OperationListItemProps) => {
     const [showPin, setShowPin] = useState(pinned);
+    const itemLabelRef = useRef<HTMLDivElement>(null);
+    const [showTooltip, setShowTooltip] = useState(false);
+
+    useEffect(() => {
+        const checkOverflow = () => {
+            const current = itemLabelRef.current;
+            if (current) {
+                console.log(operation.path, current.offsetWidth, current.scrollWidth)
+                const isOverflowing = current.offsetWidth < current.scrollWidth;
+                setShowTooltip(isOverflowing);
+            }
+        }
+
+        checkOverflow();
+    }, [])
+
+    const getItemLabelComponent = () => {
+        return (
+            <>
+                {operation.operation.summary && !onlyDisplayTechnicalNames
+                    ?
+                    <Typography component='span' noWrap>{operation.operation.summary}</Typography>
+                    :
+                    <OperationLabel operation={operation} mode="technical" alignPaths={true} />}
+                {displayApiSpec &&
+                    <Typography component='span' noWrap level="body-xs" sx={{ ml: 1 }}>
+                        {operation.apiSpec.document.info.title || 'Untitled API'}
+                    </Typography>}
+            </>
+        )
+    }
 
     return (
         <ListItem
-            endAction={showPin || pinned ? <IconButton size="sm" onClick={onPin} title={pinned ? "Unpin" : "Pin"}><PushPin /></IconButton> : undefined}
+            endAction={(showPin || pinned) &&
+                <IconButton size="sm" onClick={onPin} title={pinned ? "Unpin" : "Pin"}>
+                    <PushPin />
+                </IconButton>}
             onMouseEnter={() => setShowPin(true)}
             onMouseLeave={() => setShowPin(false)}
             sx={{ px: 2 }}>
@@ -36,17 +70,20 @@ export const OperationListItem = ({
                 selected={selected}
                 onClick={onClick}>
                 <ListItemContent>
-                    <Typography overflow='hidden' textOverflow='ellipsis'>
-                        {operation.operation.summary && !onlyDisplayTechnicalNames
-                            ?
-                            <Typography component='span' noWrap>{operation.operation.summary}</Typography>
-                            :
-                            <OperationLabel operation={operation} mode="technical" alignPaths={true} />}
-                        {displayApiSpec &&
-                            <Typography component='span' noWrap level="body-xs" sx={{ ml: 1 }}>
-                                {operation.apiSpec.document.info.title || 'Untitled API'}
-                            </Typography>}
-                    </Typography>
+                    <Tooltip
+                        describeChild
+                        disableFocusListener={!showTooltip}
+                        disableHoverListener={!showTooltip}
+                        enterDelay={500}
+                        title={getItemLabelComponent()}
+                        variant='outlined'>
+                        <Typography
+                            ref={itemLabelRef}
+                            overflow='hidden'
+                            textOverflow='ellipsis'>
+                            {getItemLabelComponent()}
+                        </Typography>
+                    </Tooltip>
                 </ListItemContent>
             </ListItemButton>
         </ListItem>
