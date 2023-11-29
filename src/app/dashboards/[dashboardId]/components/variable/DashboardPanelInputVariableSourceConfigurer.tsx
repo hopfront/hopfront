73bdb35461c13cae05b61735c4fbafd66d0b3e43
Anthony Mamode <anthony.mamode@witick.io>
2023-11-29 15:15:30 +0100

    Show create variable button only when there is no variable available yet. If not show add button in select options.

--- src/app/dashboards/[dashboardId]/components/variable/DashboardPanelInputVariableSourceConfigurer.tsx
+++ src/app/dashboards/[dashboardId]/components/variable/DashboardPanelInputVariableSourceConfigurer.tsx
@@ -1,14 +1,15 @@
-import React, {useState} from "react";
-import {Select, Option, Button, Stack} from "@mui/joy";
+import React, { useState } from "react";
+import { Select, Option, Button, Stack, Box, Divider } from "@mui/joy";
 import {
     DashboardPanelInputSourceConfigDataVariable
 } from "@/app/lib/model/dashboard/DashboardPanelInputSourceConfigDataVariable";
-import {Dashboard} from "@/app/lib/model/dashboard/Dashboard";
+import { Dashboard } from "@/app/lib/model/dashboard/Dashboard";
 import Typography from "@mui/joy/Typography";
 import {
     CreateDashboardVariableModal
 } from "@/app/dashboards/[dashboardId]/components/variable/CreateDashboardVariableModal";
-import {DashboardVariable} from "@/app/lib/model/dashboard/DashboardVariable";
+import { DashboardVariable } from "@/app/lib/model/dashboard/DashboardVariable";
+import { Create } from "@mui/icons-material";
 
 export interface DashboardPanelInputVariableSourceConfigurerProps {
     dashboard: Dashboard
@@ -19,36 +20,50 @@ export interface DashboardPanelInputVariableSourceConfigurerProps {
 }
 
 export const DashboardPanelInputVariableSourceConfigurer = ({
-                                                                dashboard,
-                                                                inputName,
-                                                                data,
-                                                                onChange,
-                                                                onVariableCreated
-                                                            }: DashboardPanelInputVariableSourceConfigurerProps) => {
+    dashboard,
+    inputName,
+    data,
+    onChange,
+    onVariableCreated
+}: DashboardPanelInputVariableSourceConfigurerProps) => {
     const [variableCreationOpen, setVariableCreationOpen] = useState(false);
-
 
     return (
         <>
             <Stack direction="row">
-                {(dashboard.variables || []).length > 0 && <Select
-                    sx={{
-                        minWidth: 200
-                    }}
-                    value={data.variableName}
-                    onChange={(_, value) => {
-                        value && onChange({variableName: value});
-                    }}>
-                    {dashboard.variables.map(v => {
-                        return (<Option key={v.name} value={v.name}>{v.name}</Option>);
-                    })}
-                </Select>}
-                <Button
-                    sx={{ml: 1}}
+                {(dashboard.variables || []).length > 0 &&
+                    <Select
+                        sx={{
+                            minWidth: 200
+                        }}
+                        value={data.variableName}
+                        onChange={(_, value) => {
+                            value && onChange({ variableName: value });
+                        }}>
+                        {dashboard.variables.map(v => {
+                            return (
+                                <Option key={v.name} value={v.name}>
+                                    {v.name}
+                                </Option>);
+                        })}
+                        {dashboard.variables.length > 0 &&
+                            <>
+                                <Divider sx={{ my: 0.5 }} />
+                                <Option value={''} onClick={() => setVariableCreationOpen(true)}>
+                                    <Stack direction='row' justifyContent='space-between' width='100%' alignItems='center'>
+                                        <Typography noWrap>Create variable</Typography>
+                                        <Create />
+                                    </Stack>
+
+                                </Option>
+                            </>}
+                    </Select>}
+                {dashboard.variables.length === 0 && <Button
+                    sx={{ ml: 1 }}
                     variant="outlined"
                     onClick={() => setVariableCreationOpen(true)}>
                     <Typography noWrap>Create variable</Typography>
-                </Button>
+                </Button>}
             </Stack>
             <CreateDashboardVariableModal
                 open={variableCreationOpen}
@@ -58,7 +73,7 @@ export const DashboardPanelInputVariableSourceConfigurer = ({
                     onVariableCreated(variable);
                 }}
                 dashboardTitle={dashboard.title}
-                inputName={inputName}/>
+                inputName={inputName} />
         </>
     );
 
