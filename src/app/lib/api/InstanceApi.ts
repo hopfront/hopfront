7ee821420645971410e38a6acc4e7c126b9842a9
Anthony Mamode <anthony.mamode@witick.io>
2023-12-18 17:26:07 +0100

    Rename endpoint PUT admin-status to PUT admin-auth

--- src/app/lib/api/InstanceApi.ts
+++ src/app/lib/api/InstanceApi.ts
@@ -2,7 +2,7 @@ import { mutateAdminStatus } from "@/app/hooks/useAdminStatus";
 
 export class InstanceApi {
     public static async updateAdminPassword(updateAdminPasswordRequest: InstanceAdminPasswordRequest) {
-        return fetch('/api/instance/admin-status/setups', {
+        return fetch('/api/instance/admin-auth/setups', {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
